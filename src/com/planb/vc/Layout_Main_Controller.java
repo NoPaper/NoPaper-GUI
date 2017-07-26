95f1b2bfebf2c69067e5f2307bdc148a11e2d917
JoMingyu <city7310@naver.com>
2017-07-26 09:24:45 +0900

    fxml 정의와 이벤트 처리 메소드 틀

--- src/com/planb/vc/Layout_Main_Controller.java
+++ src/com/planb/vc/Layout_Main_Controller.java
@@ -3,11 +3,36 @@ package com.planb.vc;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 
 public class Layout_Main_Controller implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		System.out.println("Hello");
+		
+	}
+	
+	public void openFileButtonOnAction(ActionEvent e) {
+		// Dialog show
+	}
+	
+	public void sendButtonOnAction(ActionEvent e) {
+		
+	}
+	
+	private boolean isPaired() {
+		return true;
+	}
+	
+	private void getKeyFromServer() {
+		// 서버에 파일을 전송하고 고유 id 받기
+	}
+	
+	private void sendViaBeacon() {
+		if(!isPaired()) {
+			return;
+		}
+		
+		// 비콘을 통해 전송
 	}
 }
